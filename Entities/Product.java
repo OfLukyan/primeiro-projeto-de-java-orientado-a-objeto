d53e428532337f7f66dc6628554872555bd20341
OfLukyan <mickaellira052@gmail.com>
2025-03-23 15:18:33 -0300

    Update Product.java
    melhora nas entities

--- Entities/Product.java
+++ Entities/Product.java
@@ -16,4 +16,7 @@ public class Product {
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
     }
+    public String toString() {
+        return name + ", $ " + price + ", " + quantity + " units, total: $ " + totalValueInStock();
+    }
 }
